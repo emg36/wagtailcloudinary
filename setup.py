1f6fa3ba5e3beb5a947a35bbacf07866e55d7993
emg36 <elliotgilmore@gmail.com>
2018-10-16 21:50:15 +1300

    version 0.3.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='wagtailcloudinary',
-    version='0.1',
+    version='0.3.1',
     packages=find_packages(),
     install_requires=['cloudinary', 'wagtail'],
     include_package_data=True,
